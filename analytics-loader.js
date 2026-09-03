94f9bfc245ec8c40ff4e9cfbaeb71f7d9dce8728
func <jinguodian@gmail.com>
2026-09-04 00:20:12 +0800

    feat: add Baidu Tongji loader

---
+++ analytics-loader.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fb637bf14df14904c4ad987b08f39e54";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
