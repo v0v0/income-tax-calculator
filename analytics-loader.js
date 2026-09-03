7b11b1e5ad47516078ef0c5eeaca937388365712
func <jinguodian@gmail.com>
2026-09-04 06:39:54 +0800

    load final title format override

--- analytics-loader.js
+++ analytics-loader.js
@@ -1,5 +1,10 @@
 var _hmt = _hmt || [];
 (function() {
+  var titleFormat = document.createElement("script");
+  titleFormat.src = "./title-format.js?v=20260904-1";
+  titleFormat.defer = true;
+  document.head.appendChild(titleFormat);
+
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?fb637bf14df14904c4ad987b08f39e54";
   var s = document.getElementsByTagName("script")[0];
